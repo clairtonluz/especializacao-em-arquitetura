0b60497173c7e1a464f0340996a932f5acbc1500
clairtonluz <clairton.c.l@gmail.com>
2015-09-09 22:18:30 -0300

    implementando refatoração de código louco

---
+++ src/main/java/io/github/clairtonluz/especializacao_em_arquitetura/programacao_efetiva_com_orientacao_a_objectos/aulas/organizacao/Contact.java
@@ -0,0 +1,9 @@
+package io.github.clairtonluz.especializacao_em_arquitetura.programacao_efetiva_com_orientacao_a_objectos.aulas.organizacao;
+
+public class Contact {
+	Name firstName;
+	Name lastName;
+	Phone fax;
+	Phone tel;
+	Phone mobile;
+}
---
+++ src/main/java/io/github/clairtonluz/especializacao_em_arquitetura/programacao_efetiva_com_orientacao_a_objectos/aulas/organizacao/Name.java
@@ -0,0 +1,22 @@
+package io.github.clairtonluz.especializacao_em_arquitetura.programacao_efetiva_com_orientacao_a_objectos.aulas.organizacao;
+
+public class Name {
+	private String english;
+	private String chinese;
+
+	public String getEnglish() {
+		return english;
+	}
+
+	public void setEnglish(String english) {
+		this.english = english;
+	}
+
+	public String getChinese() {
+		return chinese;
+	}
+
+	public void setChinese(String chinese) {
+		this.chinese = chinese;
+	}
+}
