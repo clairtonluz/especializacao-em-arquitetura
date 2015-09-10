13361645a32e4ea6f22d41f93119225cc21ea6da
clairtonluz <clairton.c.l@gmail.com>
2015-09-09 21:40:32 -0300

    testando cavaleiro

---
+++ src/main/java/io/github/clairtonluz/especializacao_em_arquitetura/programacao_efetiva_com_orientacao_a_objectos/aulas/cavaleiros/TestarCavaleiro.java
@@ -0,0 +1,10 @@
+package io.github.clairtonluz.especializacao_em_arquitetura.programacao_efetiva_com_orientacao_a_objectos.aulas.cavaleiros;
+
+public class TestarCavaleiro {
+
+	public static void main(String[] args) {
+		Cisne cisne = new Cisne();
+		cisne.dispararPoDeDiamante();
+		cisne.dispararTrovaoAurora();
+	}
+}
