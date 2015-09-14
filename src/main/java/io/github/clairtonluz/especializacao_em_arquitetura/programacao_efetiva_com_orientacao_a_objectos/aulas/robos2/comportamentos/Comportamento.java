d8e92d8cd024d915c96ce46ce1db55012c60fae0
clairtonluz <clairton.c.l@gmail.com>
2015-09-14 20:06:48 -0300

    implementando exercicio dos comportamentos

---
+++ src/main/java/io/github/clairtonluz/especializacao_em_arquitetura/programacao_efetiva_com_orientacao_a_objectos/aulas/robos2/comportamentos/Comportamento.java
@@ -0,0 +1,6 @@
+package io.github.clairtonluz.especializacao_em_arquitetura.programacao_efetiva_com_orientacao_a_objectos.aulas.robos2.comportamentos;
+
+public interface Comportamento {
+	void atacar();
+	void defender();
+}
