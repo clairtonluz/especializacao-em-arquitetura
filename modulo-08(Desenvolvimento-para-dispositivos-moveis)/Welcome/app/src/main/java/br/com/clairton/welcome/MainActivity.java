9318552c29d6e4a255ba4339975ae708206b01c4
clairtonluz <clairton.c.l@gmail.com>
2016-05-04 20:51:33 -0300

    iniciando projeto android de exemplo

---
+++ modulo-08(Desenvolvimento-para-dispositivos-moveis)/Welcome/app/src/main/java/br/com/clairton/welcome/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.clairton.welcome;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by clairton on 04/05/16.
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
