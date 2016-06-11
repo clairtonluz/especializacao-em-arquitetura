9e869727a15c22a69c2ef2c8be954bd631cb4c9a
clairtonluz <clairton.c.l@gmail.com>
2016-06-11 10:47:34 -0300

    exercicio 3

--- modulo-09(aplicacoes-ricas)/aulas/aula3/exercicio3/application.js
+++ modulo-09(aplicacoes-ricas)/aulas/aula3/exercicio3/application.js
@@ -1,2 +1,7 @@
-$(document).ready(function(){
+$(document).ready(function () {
+    $("button").on("click", function () {
+        var value = $("<p>R$ 1,99</p>");
+        $("button").after(value);
+        $("button").remove();
+    });
 });
