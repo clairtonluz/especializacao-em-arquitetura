4b1134111cc906c609f16d0acb42db84eca203e1
clairtonluz <clairton.c.l@gmail.com>
2016-06-11 10:59:01 -0300

    random price

--- modulo-09(aplicacoes-ricas)/aulas/aula3/exercicio4/application.js
+++ modulo-09(aplicacoes-ricas)/aulas/aula3/exercicio4/application.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     $("button").on("click", function () {
-        var value = $("<p>R$ 1,99</p>");
+        var preco = (Math.random() * 5).toFixed(2);
+        var value = $("<p>R$ " + preco + "</p>");
         $(this).after(value);
         $(this).remove();
     });
