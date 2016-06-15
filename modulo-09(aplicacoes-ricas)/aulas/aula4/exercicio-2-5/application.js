5623d4fcafc66247e834be8161aa51486ec2a386
clairtonluz <clairton.c.l@gmail.com>
2016-06-15 20:57:32 -0300

    exercicio 5

--- modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-5/application.js
+++ modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-5/application.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-    
+
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        $.ajax('pedidos.html', {
+            type: 'POST',
+            success: function (response) {
+                $('form').remove()
+                $('#pedido').hide().html(response).fadeIn();
+            }
+        });
+    });
 });
