d57ef5852f40c85a86b9faca0131eb1276ec41aa
clairtonluz <clairton.c.l@gmail.com>
2016-06-15 20:45:27 -0300

    exercicio 4

--- modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-4/application.js
+++ modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-4/application.js
@@ -4,16 +4,28 @@ function Receita(el) {
 
     var solicitacao = this;
 
-    this.carregamentoOk = function() {
-        
-    }
+    this.carregamentoOk = function () {
+        var receita = $(this).closest('.bebida').find(".receita");
 
-    this.exibirFoto = function(event) {
-    }
+        $.ajax('http://localhost:63342/aulas/aula4/exercicio-2-4/receita.html',
+            {
+                context: solicitacao,
+                success: function (response) {
+                    this.receita.html(response).slideDown();
+                }
+            });
+    };
+
+    this.exibirFoto = function (event) {
+        event.preventDefault();
+        solicitacao.receita.find("img").show();
+    };
+
+    this.el.on('click', 'button', this.carregamentoOk);
+    this.el.on('click', '.expandir', this.exibirFoto);
+
 
 }
-
-
 
 $(document).ready(function () {
     var limao = new Receita($("#limao"));
