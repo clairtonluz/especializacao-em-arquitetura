327c877108784323d46da84022ad6e1a97ed3470
clairtonluz <clairton.c.l@gmail.com>
2016-06-15 22:04:02 -0300

    exercicio 7 e 8

--- modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-7/application.js
+++ modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-7/application.js
@@ -1,3 +1,23 @@
 $(document).ready(function () {
 
+    $('button').on('click', function () {
+        $.ajax('bebidas/favoritas/1.json', {
+            dataType: 'json',
+            success: function (response) {
+                // $.each(response, function (index, bebida) {
+                //     var destaque = $('.destaque-' + index);
+                //     destaque.find('p').html(bebida.nome);
+                //     destaque.find('img').attr('src', bebida.imagem);
+                // });
+
+                $.map(response, function (bebida, index) {
+                    var destaque = $('.destaque-' + index);
+                    destaque.find('p').html(bebida.nome);
+                    destaque.find('img').attr('src', bebida.imagem);
+                    return response;
+                });
+            },
+            contentType: 'application/json'
+        });
+    })
 });
--- modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-8/application.js
+++ modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-8/application.js
@@ -1,2 +1,28 @@
 $(document).ready(function () {
+    $('button').on('click', function () {
+        $.ajax('bebidas/favoritas/1.json', {
+            dataType: 'json',
+            success: function (response) {
+                $.each(response, function (index, bebida) {
+                    console.log(index);
+                    var destaque = $('.destaque-' + index);
+                    destaque.find('p').html(bebida.nome);
+                    destaque.find('img').attr('src', bebida.imagem);
+                });
+            },
+            contentType: 'application/json'
+        });
+
+        // $.getJSON('status/todos.json', function (result) {
+        //     $.map(result, function (item, indice) {
+        //         var itemPedido = $("<li></li>");
+        //         $('<h3>' + item.nome + '</h3>').appendTo(itemPedido);
+        //         return itemPedido;
+        //     });
+        //
+        //     console.log(result);
+        //     $('ul').html(result);
+        // });
+    });
+
 });
