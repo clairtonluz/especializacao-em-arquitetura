4fdc627c045685244659a9ae2e332853fafc7d25
clairtonluz <clairton.c.l@gmail.com>
2016-06-15 21:18:19 -0300

    exercicio 6

--- modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-6/application.js
+++ modulo-09(aplicacoes-ricas)/aulas/aula4/exercicio-2-6/application.js
@@ -1,2 +1,20 @@
 $(document).ready(function () {
+
+    $('form').on('submit', function (event) {
+        event.preventDefault();
+        $.ajax($('form').attr('action'), {
+            type: 'POST',
+            dataType: 'json',
+            success: function (response) {
+                $('form').remove()
+                var result = "Pedido:" + response.pedido
+                    + ", Sanduiche: " + response.sanduiche
+                    + ", Quantidade: " + response.quantidade
+                    + ", Total: " + response.total;
+                // var result = JSON.stringify(response).replaceAll('{', '');
+                $('#pedido').hide().html(result).fadeIn();
+            },
+            contentType: 'application/json'
+        });
+    });
 });
