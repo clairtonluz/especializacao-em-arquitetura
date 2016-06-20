49bc3e9c05dc6076bf2751a2a1f0866f16eee433
clairtonluz <clairton.c.l@gmail.com>
2016-06-20 20:58:20 -0300

    ...

--- modulo-09(aplicacoes-ricas)/aulas/aula5/angularjs/js/app.js
+++ modulo-09(aplicacoes-ricas)/aulas/aula5/angularjs/js/app.js
@@ -17,16 +17,27 @@
         }
     });
 
+    app.controller('ComentarioControle', function () {
+        this.comentario = {};
+
+        this.addComentario = function (livro) {
+            livro.comentarios.push(this.comentario);
+            this.comentario = {};
+        };
+    });
+
     var livros = [{
         titulo: 'O senhor  dos Aneis',
         preco: 45.6,
         descricao: 'Descricao 1',
-        quantidade: 10
+        quantidade: 10,
+        comentarios: [{autor: 'clairton', texto: 'texto 1'}, {autor: 'fulano', texto: 'texto 2'}]
     }, {
         titulo: 'A Saga Star Wars',
         preco: 35.6,
         descricao: 'Descricao 2',
-        quantidade: 0
+        quantidade: 0,
+        comentarios: [{autor: 'autor 1', texto: 'texto 1'}, {autor: 'autor 2', texto: 'texto 2'}]
     }];
 })();
 
